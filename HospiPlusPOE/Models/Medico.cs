67a02546b44a503899e427527ffb784ac33ff1c4
José Mejia <josemejia.cto@gmail.com>
2024-11-19 01:46:48 -0600

    Ventana admin + Nuget:FontAwesome

---
+++ HospiPlusPOE/Models/Medico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospiPlusPOE.Models
+{
+    public class Medico : Usuario
+    {
+        public int ID_Medico { get; set; }
+        public string Especialidad { get; set; }
+        public string NumeroLicencia { get; set; }
+        public string Estado { get; set; }
+    }
+}
---
+++ HospiPlusPOE/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospiPlusPOE.Models
+{
+    public class Usuario
+    {
+        public int ID_Usuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Rol { get; set; }
+        public string Nickanme { get; set; }
+        public string Telefono { get; set; }
+        public string Password { get; set; }
+
+    }
+}
